cd3d19eea5ce7fa0c374485393395c7eff724b2e
Jack Myers <jack.myers@intel.com>
2024-07-31 11:46:13 -0700

    fish no longer is orphaned process

--- user-init-scripts/config.fish
+++ user-init-scripts/config.fish
@@ -2,6 +2,6 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     if test "$SKIP_FISH_CONFIG" != "1"
 	set -x SKIP_FISH_CONFIG 1
-	bash -i -c "fish"
+	exec bash -i -c "fish"
     end
 end
